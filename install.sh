9429891fd40dbce62e2f3f9e318a99ceb4649972
Moexin <moexin@qq.com>
2021-01-16 15:07:02 +0800

    Update

--- install.sh
+++ install.sh
@@ -37,4 +37,38 @@ export EDITOR="/usr/bin/vim.tiny" ;
 crontab -e <<EOF
 :wq
 EOF
-echo '转发安装完成'
+echo '正在安装内核'
+while true;do wget -T 15 -c -q https://raw.sevencdn.com/Moexin/BBRPlus/master/linux-headers-4.14.129-bbrplus.deb && break;done
+dpkg -i linux-headers-4.14.129-bbrplus.deb > /dev/null
+rm -f linux-headers-4.14.129-bbrplus.deb
+while true;do wget -T 15 -c -q https://raw.sevencdn.com/Moexin/BBRPlus/master/linux-image-4.14.129-bbrplus.deb && break;done
+dpkg -i linux-image-4.14.129-bbrplus.deb > /dev/null
+rm -f linux-image-4.14.129-bbrplus.deb
+echo '优化系统配置'
+cat << EOF >> /etc/sysctl.conf
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbrplus
+net.ipv4.tcp_retries2 = 8
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_fastopen = 3
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+# forward ipv4
+net.ipv4.ip_forward = 1
+EOF
+sysctl -p > /dev/null
+echo '重启系统生效'
+reboot
--- uninstall.sh
+++ uninstall.sh
@@ -15,4 +15,32 @@ export EDITOR="/usr/bin/vim.tiny" ;
 crontab -e <<EOF
 :wq
 EOF
-echo '转发卸载完成'
+echo '删除优化配置'
+sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_retries2/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf
+sed -i '/fs.file-max/d' /etc/sysctl.conf
+sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
+sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
+sed -i '/forward ipv4/d' /etc/sysctl.conf
+sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+echo '正在卸载内核'
+apt-get --purge autoremove linux-headers-4.14.129-bbrplus -y > /dev/null
+apt-get --purge autoremove linux-image-4.14.129-bbrplus -y > /dev/null
+update-grub
+echo '重启系统生效'
+reboot
